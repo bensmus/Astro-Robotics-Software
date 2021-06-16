49620026f7be57f3b790e10efd265a1872dbf2ba
bensmus <bensmus@gmail.com>
2021-06-16 01:28:13 -0700

    refactoring

--- obstacle_game/obstacle_creation.py
+++ obstacle_game/obstacle_creation.py
@@ -1,21 +1,3 @@
-'''
-Coordinate system is (x, y),    y       x --->
-                                |
-                                |
-                                |
-                                ∨
-'''
-'''
-OBSTACLE CREATION TECHNIQUE
-1.  Choose "n" random points in the worldsize
-2.  Create bounding rectangles for all "n" points
-3.  Do a chain circle growing technique from the middle until we create a bulbuous shape that touches the bounding rectangle,
-    or until 5 iterations
-'''
-
-# Version 1 simplifies this obstacle creation technique, to have all obstacles be rectangles.
-# TODO: Refactor into object oriented for rectangle class
-
 import pygame
 import random
 pygame.init()
@@ -39,24 +21,59 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    def get_bound_pts(self):
+        """
+        Get the points on the edge of the rectangle.
+        """
 
-def get_bound_pts(topleft, width, height):
-    """
-    Get the points on the edge of the rectangle.
-    """
+        topleft = self.topleft
+        width = self.width
+        height = self.height
 
-    topright = Point(topleft.x + width, topleft.y)
-    bottomleft = Point(topleft.x, topleft.y + height)
-    bottomright = Point(topleft.x + width, topleft.y + height)
+        topright = Point(topleft.x + width, topleft.y)
+        bottomleft = Point(topleft.x, topleft.y + height)
+        bottomright = Point(topleft.x + width, topleft.y + height)
 
-    bound_pts = []
-    bound_pts.extend((topleft, topright, bottomleft, bottomright))
-    bound_pts.extend(get_line_pts(topleft, topright, True))
-    bound_pts.extend(get_line_pts(topright, bottomright, False))
-    bound_pts.extend(get_line_pts(bottomleft, bottomright, True))
-    bound_pts.extend(get_line_pts(topleft, bottomleft, False))
+        bound_pts = []
+        bound_pts.extend((topleft, topright, bottomleft, bottomright))
+        bound_pts.extend(get_line_pts(topleft, topright, True))
+        bound_pts.extend(get_line_pts(topright, bottomright, False))
+        bound_pts.extend(get_line_pts(bottomleft, bottomright, True))
+        bound_pts.extend(get_line_pts(topleft, bottomleft, False))
 
-    return bound_pts
+        return bound_pts
+
+    def get_not_in_func(self):
+        """
+        Return a function that takes in a point, and checks if its contained within a rectangle.
+        """
+        topleft = self.topleft
+        width = self.width
+        height = self.height
+
+        def f(point):
+            min_x = topleft.x
+            min_y = topleft.y
+            max_x = min_x + width
+            max_y = min_y + height
+            if point.y > min_y and point.y < max_y and point.x > min_x and point.x < max_x:
+                return False
+            return True
+        return f
+
+    def get_set_all_points(self):
+        """
+        Get all the points of the rectangle.
+        """
+        topleft = self.topleft
+        width = self.width
+        height = self.height
+
+        points = set()
+        for x in range(topleft.x, topleft.x + width + 1):
+            for y in range(topleft.y, topleft.y + height + 1):
+                points.add((x, y))
+        return points
 
 
 def get_line_pts(point_a, point_b, horiz):
@@ -72,32 +89,6 @@ def get_line_pts(point_a, point_b, horiz):
         for ycoor in range(point_a.y + 1, point_b.y):
             pts.append(Point(point_a.x, ycoor))
     return pts
-
-
-def not_in_rect(topleft, width, height):
-    """
-    Return a function that takes in a point, and checks if its contained within a rectangle.
-    """
-    def f(point):
-        min_x = topleft.x
-        min_y = topleft.y
-        max_x = min_x + width
-        max_y = min_y + height
-        if point.y > min_y and point.y < max_y and point.x > min_x and point.x < max_x:
-            return False
-        return True
-    return f
-
-
-def get_set_all_points(topleft, width, height):
-    """
-    Get all the points of the rectangle.
-    """
-    points = set()
-    for x in range(topleft.x, topleft.x + width + 1):
-        for y in range(topleft.y, topleft.y + height + 1):
-            points.add((x, y))
-    return points
 
 
 def reset_start_dest(spawn_pts, start, dest):
@@ -162,17 +153,17 @@ if __name__ == '__main__':
         # Drawing the black rectangle.
         pygame.draw.rect(screen, BLACK, (topleft.x, topleft.y, width, height))
 
-        rects.append(Rectangle(topleft, width, height))
-        spawn_pts -= get_set_all_points(topleft, width, height)
-        bound_pts = get_bound_pts(topleft, width, height)
+        rect = Rectangle(topleft, width, height)
+        rects.append(rect)
+        spawn_pts -= rect.get_set_all_points()
+        bound_pts = rect.get_bound_pts()
 
         wall_pts.extend(bound_pts)
 
     # DO THIS OUTSIDE THE LOOP, WHEN ALL RECTS DEFINED
     # Filter the bound points. If they are inside an obstacle, they are removed.
     for rect in rects:
-        wall_pts = list(filter(not_in_rect(
-            rect.topleft, rect.width, rect.height), wall_pts))
+        wall_pts = list(filter(rect.get_not_in_func(), wall_pts))
 
     # Draw the walls.
     for point in wall_pts:
@@ -201,5 +192,6 @@ if __name__ == '__main__':
         x, y = pygame.mouse.get_pos()
         print(f'mouse coordinates x = {x:-3}, y = {y:-3}', end='\r')
         pygame.display.update()
+
     # Done! Time to quit.
     pygame.quit()
